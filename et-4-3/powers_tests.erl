603ea5c9d523aa35f1fee07ff067a7a6059bc601
Craig Ferry <craig@puritycontrol.net>
2015-12-21 09:52:27 +0000

    Complete etudes 4-3.

---
+++ et-4-3/powers_tests.erl
@@ -0,0 +1,20 @@
+-module(powers_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+base_case_test() ->
+    ?assertEqual(5, powers:raise(5, 1)).
+
+raise_to_zero_test() ->
+    ?assertEqual(1, powers:raise(2, 0)).
+
+positive_power_test() ->
+    ?assertEqual(8, powers:raise(2, 3)).
+
+negative_power_test() ->
+    ?assertEqual(0.125, powers:raise(2, -3)).
+
+float_power_test() ->
+    ?assertEqual(1.728, powers:raise(1.2, 3)).
+
+
