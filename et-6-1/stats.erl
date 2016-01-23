968c81f934e587124713333b499933d3874776c8
PurityControl <PurityControl@users.noreply.github.com>
2016-01-23 08:56:06 +0000

    Add documetation and spec signature for minimum.

--- et-6-1/stats.erl
+++ et-6-1/stats.erl
@@ -1,7 +1,12 @@
+%% @author Purity Control
+%% @vsn 0.1
+%% @doc an implementation of getting the minimum value from a list
 -module(stats).
 
 -export([minimum/1]).
 
+%% doc return the minimum in a list. Throws an error on the empty list
+-spec(minimum(nonempty_list(any())) -> any()).
 minimum([H | T]) ->
     minimum(T, H).
 
